e66c3899becc968347537b98533d14397d51b078
lkmmethy <lkmmethy@DESKTOP-0BEGPN0>
2022-06-26 22:03:19 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace temp
+{
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Console.ReadLine().Count(x => char.IsDigit(x)));
+        }
+
+    }
+}
